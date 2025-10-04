4d3542c289de1cb9463e3fd8f6d9af09c5ec5770
cayuse <cayuse@mac.com>
2025-10-04 16:52:28 -0700

    migration hell

--- archive/db/migrate/20250124000001_create_jukebox_playlist_assignments.rb
+++ archive/db/migrate/20250124000001_create_jukebox_playlist_assignments.rb
@@ -9,10 +9,10 @@ class CreateJukeboxPlaylistAssignments < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :jukebox_playlist_assignments, :jukebox_id
-    add_index :jukebox_playlist_assignments, :playlist_id
-    add_index :jukebox_playlist_assignments, [:jukebox_id, :playlist_id], unique: true
-    add_index :jukebox_playlist_assignments, [:jukebox_id, :enabled]
+    add_index :jukebox_playlist_assignments, :jukebox_id, name: 'idx_jpa_jukebox_id'
+    add_index :jukebox_playlist_assignments, :playlist_id, name: 'idx_jpa_playlist_id'
+    add_index :jukebox_playlist_assignments, [:jukebox_id, :playlist_id], unique: true, name: 'idx_jpa_unique_assignment'
+    add_index :jukebox_playlist_assignments, [:jukebox_id, :enabled], name: 'idx_jpa_jukebox_enabled'
 
     add_foreign_key :jukebox_playlist_assignments, :jukeboxes, column: :jukebox_id, on_delete: :cascade
     add_foreign_key :jukebox_playlist_assignments, :playlists, column: :playlist_id, on_delete: :cascade
